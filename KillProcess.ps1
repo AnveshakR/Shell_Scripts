192ef0b867d7cffafdbc9d47c95e98e15abae77c
AnveshakR <rathoreanveshak@gmail.com>
2022-11-18 17:36:57 -0500

    first commit

---
+++ KillProcess.ps1
@@ -0,0 +1,18 @@
+﻿[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+
+$title = 'Kill process'
+$msg   = 'Enter process name:'
+
+$text = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
+
+$process = Get-Process -Name *$text*
+
+$process | Get-Process | Format-Table ProcessName, Id -AutoSize
+
+$id = [Microsoft.VisualBasic.Interaction]::InputBox("Which Id" ,"Enter Id number")
+
+if($id -eq "all")
+{Get-Process -Name $text | Stop-Process -Force}
+
+else
+{Get-Process -Id $id | Stop-Process -Force}
---
+++ obsclip.ps1
@@ -0,0 +1,35 @@
+﻿$allow_obs_outside = $true
+$launch_obs_once = $false
+
+while ($true) 
+{
+    $r6 = Get-Process firefox -ErrorAction SilentlyContinue
+    $obs = Get-Process obs64 -ErrorAction SilentlyContinue
+    if ($r6)
+    {
+        if ($obs)
+        {
+            continue
+        }
+        else 
+        {
+            $allow_obs_outside = $true
+            Start-Process -FilePath "C:\Program Files\obs-studio\bin\64bit\obs64.exe" -WorkingDirectory "C:\Program Files\obs-studio\bin\64bit" -ArgumentList "--scene r6", "--startreplaybuffer"
+        }
+    }
+    else 
+    {
+        if ($obs -and $allow_obs_outside)
+        {
+            $obs | Stop-Process
+            $obs_folder = Get-Content ($env:APPDATA+"\obs-studio\basic\profiles\Untitled\basic.ini") | Where-Object {$_ -match "="} | ConvertFrom-StringData
+            Start-Process $obs_folder.FilePath
+            $allow_obs_outside = $false
+        }
+        else 
+        {
+            continue
+        }
+    }
+    Start-Sleep -Seconds 5
+}
